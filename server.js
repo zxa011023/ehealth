cd88a0e28920200c135d67843e855e6a505cd966
Lola <zxa011023@gmail.com>
2019-07-26 04:26:00 +0800

    update server dest

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve static files....
+// Serve static files
 app.use(express.static(__dirname + '/dist/ehealth'));
 
 // Send all requests to index.html
@@ -10,5 +10,5 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/ehealth/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
